b68c9bf2f7a9bff8e94a655985f69b937b893f67
Anshika-N-22 <nissi.anshika22@gmail.com>
2025-06-14 15:46:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,3 +13,9 @@ if st.button("Convert"):
     rate = response['rates'][to_currency]
     result = amount * rate
     st.success(f"{amount} {from_currency} = {result:.2f} {to_currency}")
+with st.expander("💬 Open CareerAI Chatbot"):
+    st.components.v1.iframe(
+        "https://hf.co/chat/assistant/684d3f7c795c4b019162d4f7",
+        height=700,
+        width=900
+    )
